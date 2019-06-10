9a9bbedecd1c2f62301471150b35068ca9b9643d
Jaehoon Oh <ojh420@gmail.com>
2019-06-11 06:22:47 +0900

    Change

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var UserRepository = require('../domain/users/UserRepository')
+var UserService = require('../domain/users/UserService');
+
+const userRepository = new UserRepository();
+const userService = new UserService(userRepository);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
